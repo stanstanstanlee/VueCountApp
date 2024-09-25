5780e73d3aa4bab6c931b44ee88dfd255094e4a2
Jin Lee <127020275+stanstanstanlee@users.noreply.github.com>
2024-09-25 10:37:53 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from "vue"
+const count = ref(0);
+
+const addToCount = () => {
+    console.log("hello from addToCount")
+    count.value = count.value + 1;
+}
+
+const subtractFromCount = () => {
+  console.log("hello from subtractFromCount")
+  count.value = count.value - 1; 
+}
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is ...</h4>
+      <h1>
+        {{ count }}
+      </h1>
+      <button @click="subtractFromCount">-</button>
+      <button @click="addToCount()">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    background-color: aliceblue;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  div {
+    text-align: center;
+  }
+</style>
